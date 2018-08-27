bfac0c4b2e5c31eab17f1ceb0a1e1d7f76ce2fa4
V Echeveste <vaeb80@gmail.com>
2018-08-27 19:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+function getFirstSelector(selector){
+  /*Accepts a selector and returns the first element that matches.*/
+  return document.querySelector(`${selector}`)
+}
+function nestedTarget(){
+  /* pulls a .target out of #nested, note that # is used for IDs in selectors. */
+  return document.querySelectorAll()
+}
+function increaseRankBy(n){
+  /*Increases the ranks in all of the .ranked-list by n, might need parseInt() */
+}
+function deepestChild(){
+  /*pulls out the most deeply nested child from div#grand-node, iterate over elements and call querySelector() and querySelectorAll()*/
+}
